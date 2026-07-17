1b00bb495d0c156fe58895c4697c105e5a6f53ae
bsoroushian <bsoroushian@gmail.com>
2026-07-17 19:16:23 -0400

    chore: exclude compiler error caches and binary runner folders

--- palisade-core/bin/main/com/palisade/ratelimit/RateLimitStore.kt
+++
@@ -1,9 +0,0 @@
-package com.palisade.ratelimit
-
-interface RateLimitStore {
-    suspend fun tryConsume(
-        clientId: String,
-        maxTokens: Long,
-        refillRatePerSecond: Long,
-    ): Boolean
-}
